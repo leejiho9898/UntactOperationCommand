5706224987c4caad9a43b1fc959e61f6ade072d8
이지호 <cctv1998@naver.com>
2021-09-06 22:43:00 +0900

    Style : 일단 Header 만들었는데 다시 만들 가능성 높음

--- frontend/src/components/base/Header.tsx
+++ frontend/src/components/base/Header.tsx
@@ -4,114 +4,82 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Header = () => {
-    const [block, setblock] = useState(false)
+  const [block, setblock] = useState(false);
   return (
-    <HeaderBox>
-      <div id="LandingHeader">
-        <div id="header">
-          <div id="search-area">
-            <div className="logo">
-              <a href="/">{/* <img src={logo} alt="" /> */}</a>
-            </div>
-            <div className="search-box">
-              <input
-                type="text"
-                className="search"
-                placeholder="검색어를 입력하세요"
-              ></input>
-              <div className="search-ico">
-                <span>
-                  <BsSearch size="18" />
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="user">
-            <div className="user-border">
-              <ul>
-                {block===true? ( 
-                <div>
-                  <li>hi, 사용자이름!</li>
-                  <li>
-                    <button>로그아웃</button>
-                  </li>
-                </div>
-                  ) : ( 
-                <div>
-                  <li>
-                    <Link to="/">로그인</Link>
-                  </li>
-                  <li>
-                    <Link to="/">회원가입</Link>
-                  </li>
-                </div>
-                 )} 
-              </ul>
-            </div>
-          </div>
-        </div>
+    <HeaderBlock>
+      <HeaderLogo>로고 들어갈 자리</HeaderLogo>
+      <div>공지사항 | 기타 등등 | 후기게시판</div>
+      <div className="header-tool">
+        <SearchButton to="/">
+          <BsSearch />
+        </SearchButton>
+        {block === false ? (
+          <>
+            <RoundButton to="/"> 로그인</RoundButton>
+            <RoundButton to="/"> 회원가입</RoundButton>
+          </>
+        ) : (
+          <>
+            <RoundButton to="/">로그아웃</RoundButton>
+          </>
+        )}
       </div>
-    </HeaderBox>
+    </HeaderBlock>
   );
 };
 
-const HeaderBox = styled.div`
-  #header {
+const HeaderBlock = styled.div`
+  height: 4rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .header-tool {
     display: flex;
-    box-shadow: 0px 4px 0px rgba(0, 0, 0, 0.08);
-    height: 60px;
-    align-items: center;
-    background-color: #fff;
-    width: 1080px;
-    margin: 0 auto;
-    #search-area {
-      display: flex;
-      justify-content: space-evenly;
-      align-items: center;
-      width: 100%;
-      height: 45px;
-      .search-box {
-        display: flex;
-        width: 400px;
-        height: 46px;
-        align-items: center;
-        .search {
-          height: 30px;
-          width: 300px;
-          border: 1px solid #999;
-          border-radius: 3px;
-        }
-        .search-ico {
-          height: 100%;
-          width: 35px;
-          font-size: 14px;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          cursor: pointer;
-          span {
-            vertical-align: middle;
-            color: black;
-          }
-        }
-      }
-      
-    }
-    .user {
-        display: flex;
-        flex-basis: 200px;
-        ul {
-          display: inline-block;
-          height: 20;
-          align-items: center;
-        }
-        li {
-          display: inline;
-          margin: 5px;
-          font-size: 13px;
-          color: black;
-        }
-      }
   }
 `;
+const HeaderLogo = styled.div`
+  margin-left: 15px;
+`;
+
+const SearchButton = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: transparent;
+  border: none;
+  width: 2.5rem;
+  height: 2.5rem;
+  outline: none;
+  border-radius: 50%;
+  cursor: pointer;
+  &:hover {
+    background: rgba(0, 0, 0, 0.045);
+  }
+  svg {
+    width: 1.125rem;
+    height: 1.125rem;
+  }
+  margin-right: 0.75rem;
+`;
+
+const RoundButton = styled(Link)`
+  display: flex;
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  width: 5.5rem;
+  height: 2.5rem;
+  outline: none;
+  border-radius: 20px;
+  cursor: pointer;
+  margin: 5px;
+  background-color: #f3be4b;
+  &:hover {
+    background: #be953a;
+  }
+`;
+
 export default Header;
