f172c82a7fed89757f182151c0d607051d64fdc3
이지호 <cctv1998@naver.com>
2021-09-19 14:26:48 +0900

    modify(router)

--- backend/src/routers/userRouter.ts
+++ backend/src/routers/userRouter.ts
@@ -6,9 +6,9 @@ const router = express.Router();
 
 router.post("/register", userCtrl.register);
 router.post("/login", userCtrl.login);
-router.post("/logout", userCtrl.logout);
-router.post("/addFriend", userCtrl.addFriend);
-router.delete("/deleteFriend/:friendId", userCtrl.deleteFriend);
-router.get("/readFriendList", userCtrl.readFriendList);
+router.post("/logout", checkLoggedIn, userCtrl.logout);
+router.post("/addFriend", checkLoggedIn, userCtrl.addFriend);
+router.delete("/deleteFriend/:friendId", checkLoggedIn, userCtrl.deleteFriend);
+router.get("/readFriendList", checkLoggedIn, userCtrl.readFriendList);
 router.post("/uploadImg", userCtrl.uploadImg);
 export default router;
